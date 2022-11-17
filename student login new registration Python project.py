58730753bd7aea1a0aba4aa98df9132f9ba8e7b5
AbhiCodingA <104027464+AbhiCodingA@users.noreply.github.com>
2022-11-17 12:13:11 +0530

    Add files via upload

--- student login new registration Python project.py
+++ student login new registration Python project.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
-#import modules
-
 from tkinter import *
 import os
+
 
 # Designing window for registration
 
@@ -46,7 +39,7 @@ def register():
     mobileno_lable = Label(register_screen, text="mobileno * ")
     mobileno_lable.pack()
     mobileno_entry = Entry(register_screen, textvariable=mobileno)
-    mobileno_entry.pack()    
+    mobileno_entry.pack()
     username_lable = Label(register_screen, text="Username * ")
     username_lable.pack()
     username_entry = Entry(register_screen, textvariable=username)
@@ -56,8 +49,10 @@ def register():
     password_entry = Entry(register_screen, textvariable=password, show='*')
     password_entry.pack()
     Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
-#new supervisor
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command=register_user).pack()
+
+
+# new supervisor
 def newsupervisor():
     global newsupervisor_screen
     newsupervisor_screen = Toplevel(main_screen)
@@ -93,7 +88,7 @@ def newsupervisor():
     mobileno_lable = Label(newsupervisor_screen, text="mobileno * ")
     mobileno_lable.pack()
     mobileno_entry = Entry(newsupervisor_screen, textvariable=mobileno)
-    mobileno_entry.pack()    
+    mobileno_entry.pack()
     username_lable = Label(newsupervisor_screen, text="Username * ")
     username_lable.pack()
     username_entry = Entry(newsupervisor_screen, textvariable=username)
@@ -103,10 +98,10 @@ def newsupervisor():
     password_entry = Entry(newsupervisor_screen, textvariable=password, show='*')
     password_entry.pack()
     Label(newsupervisor_screen, text="").pack()
-    Button(newsupervisor_screen, text="Register", width=10, height=1, bg="blue", command = newsupervisor_user).pack()
+    Button(newsupervisor_screen, text="Register", width=10, height=1, bg="blue", command=newsupervisor_user).pack()
 
 
-# Designing window for login 
+# Designing window for login
 
 def login():
     global login_screen
@@ -130,16 +125,16 @@ def login():
     username_login_entry.pack()
     Label(login_screen, text="").pack()
     Label(login_screen, text="Password * ").pack()
-    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
     password_login_entry.pack()
     Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
 
 # Implementing event on register button
 
 def register_user():
-   # name_info = username.get()
-    #register_user_screen.geometry("650x450")
+    # name_info = username.get()
+    # register_user_screen.geometry("650x450")
     username_info = username.get()
     password_info = password.get()
 
@@ -148,15 +143,43 @@ def register_user():
     file.write(password_info)
     file.close()
 
-    
     username_entry.delete(0, END)
     password_entry.delete(0, END)
-    
+
     Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+
+def newsupervisor_user():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Supervisor Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="supervisor login", width=10, height=1, command=login_verify).pack()
+
+
 # new supervisor2
 def newsupervisor_user():
-   # name_info = username.get()
-    #register_user_screen.geometry("650x450")
+    # name_info = username.get()
+    # register_user_screen.geometry("650x450")
     username_info = username.get()
     password_info = password.get()
 
@@ -165,19 +188,20 @@ def newsupervisor_user():
     file.write(password_info)
     file.close()
 
-    
     username_entry.delete(0, END)
     password_entry.delete(0, END)
-    
+
     Label(newsupervisor_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
-# Implementing event on login button 
+
+
+# Implementing event on login button
 
 def login_verify():
     username1 = username_verify.get()
     password1 = password_verify.get()
     username_login_entry.delete(0, END)
     password_login_entry.delete(0, END)
-    
+
     list_of_files = os.listdir()
     if username1 in list_of_files:
         file1 = open(username1, "r")
@@ -191,6 +215,7 @@ def login_verify():
     else:
         user_not_found()
 
+
 # Designing popup for login success
 
 def login_sucess():
@@ -200,6 +225,7 @@ def login_sucess():
     login_success_screen.geometry("250x150")
     Label(login_success_screen, text="Login Success").pack()
     Button(login_success_screen, text="OK", command=delete_login_success).pack()
+
 
 # Designing popup for login invalid password
 
@@ -211,8 +237,9 @@ def password_not_recognised():
     Label(password_not_recog_screen, text="Invalid Password ").pack()
     Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
 
+
 # Designing popup for user not found
- 
+
 def user_not_found():
     global user_not_found_screen
     user_not_found_screen = Toplevel(login_screen)
@@ -220,6 +247,7 @@ def user_not_found():
     user_not_found_screen.geometry("150x100")
     Label(user_not_found_screen, text="User Not Found").pack()
     Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+
 
 # Deleting popups
 
@@ -244,25 +272,14 @@ def main_account_screen():
     main_screen.title("Account Login")
     Label(text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
-    Button(text="Login", height="2", width="30", command = login).pack()
+    Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
     Button(text="Register", height="2", width="30", command=register).pack()
     Label(text="").pack()
-    Button(text="New supervisor", height="2", width="30", command=newsupervisor).pack()        
+    Button(text="New supervisor", height="2", width="30", command=newsupervisor).pack()
+    Label(text="").pack()
+    Button(text="Supervisor Login",height="2", width="30", command=newsupervisor).pack()
     main_screen.mainloop()
 
 
 main_account_screen()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
